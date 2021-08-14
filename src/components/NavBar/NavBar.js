9b7af896cdb64f420724ab012682c134ff18790a
René Donaire <rene.donaire@gmail.com>
2021-08-14 14:16:42 -0400

    Instala Bootstrap

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+            <h1>LOGO</h1>
+            <nav>
+                <a href="#">Enlace 1</a>
+                <a href="#">Enlace 2</a>
+                <a href="#">Enlace 3</a>
+            </nav>
+        </div>
+    )
+}
