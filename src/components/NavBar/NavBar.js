e23b4214a49e34fc3f238c91d44fa2aa7dfe7fb8
René Donaire <rene.donaire@gmail.com>
2021-08-14 15:30:19 -0400

    Nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 
 export const NavBar = () => {
     return (
-        <div>
-            <h1>LOGO</h1>
-            <nav>
-                <a href="#">Enlace 1</a>
-                <a href="#">Enlace 2</a>
-                <a href="#">Enlace 3</a>
-            </nav>
-        </div>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#">
+                    <img
+                        src="./images/logo.png"
+                        width="100"
+                        height="40"
+                        className="d-inline-block "
+                        alt=""
+                    />
+                </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#">Novedades</Nav.Link>
+                        <Nav.Link href="#">Libros</Nav.Link>
+                        <Nav.Link href="#">Descargas</Nav.Link>
+                    </Nav>
+            </Container>
+        </Navbar>
     )
-}
+};
