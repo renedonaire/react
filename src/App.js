24fbfe265ac5a574dbe6b9345641b97afe84087f
René Donaire <rene.donaire@gmail.com>
2021-08-20 22:34:51 -0400

    Trabajo con estilos y normalize.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/styles/styles.scss';
 import { NavBar } from "./components/NavBar/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 
