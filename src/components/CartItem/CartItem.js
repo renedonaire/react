e0d9aa07e4bbea75d78dfd59b63d80e3cc64a4a2
René Donaire <rene.donaire@gmail.com>
2021-09-04 18:17:32 -0400

    Estilos carro de compra

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -6,13 +6,13 @@ export const CartItem = (prod) => {
     const { eliminarDelCarrito } = useContext(CartContext)
 
     return (
-                <Card key={prod.id} style={{ width: '18rem' }}>
+                <Card key={prod.id} style={{ width: '18rem', padding: '0.5rem'}}>
                     <Card.Img variant="top" src={prod.pictureUrl} />
-                    <Card.Body>
-                        <Card.Title>{prod.title}</Card.Title>
-                        <Card.Text>Cantidad: {prod.cantidad}</Card.Text>
-                        <Card.Text>Precio: ${prod.precio * prod.cantidad}</Card.Text>
-                        <Button variant="danger" onClick={() => eliminarDelCarrito(prod.id)} >Quitar del Carro</Button>
+            <Card.Body>
+                        <Card.Title className="cartItemText">{prod.title}</Card.Title>
+                        <Card.Text className="cartItemText">Cantidad: {prod.cantidad}</Card.Text>
+                        <Card.Text className="cartItemText">Subtotal: ${prod.price * prod.cantidad}</Card.Text>
+                        <Button variant="outline-danger" onClick={() => eliminarDelCarrito(prod.id)} >Quitar del Carro</Button>
                     </Card.Body>
                 </Card>
     )
--- src/components/CartScreen/CartScreen.js
+++ src/components/CartScreen/CartScreen.js
@@ -1,17 +1,25 @@
 import React, { useContext } from 'react'
+import { Container, Col, Row } from 'react-bootstrap'
 import { CartContext } from '../../context/CartContext'
-import { Card, Button } from 'react-bootstrap'
 import { CartItem } from '../CartItem/CartItem'
+import "./CartScreen.scss"
 
 export const CartScreen = () => {
     const { carrito, vaciarCarrito } = useContext(CartContext)
 
     return (
-        <div>
-            <h1>Resumen de compra</h1>
-
-            {carrito.map(prod => (<CartItem {...prod}/>            )            )            }
-            <button className="btn btn-danger" onClick={vaciarCarrito}>Vaciar carrito</button>
-        </div>
+        <Container>
+            <h2 className="cartScreen_title">Resumen de compra</h2>
+            <Row>
+                <Col className="cartScreen_col col-lg-8">
+                    <Row>
+                        {carrito.map((producto) => <CartItem key={producto.id} {...producto} />)}
+                    </Row>
+                </Col>
+                <Col>
+                    <button className="btn btn-danger fijarTop" onClick={vaciarCarrito}>Vaciar carrito</button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
