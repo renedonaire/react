6b4d8753bb1eb81ce8a2583d37fcbe7908ba06cd
René Donaire <rene.donaire@gmail.com>
2021-09-17 21:17:06 -0300

    Formulario checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,94 +1,100 @@
 import React, { useContext, useState } from 'react'
+import { Row, Col, Container } from 'react-bootstrap'
 import { Redirect } from 'react-router'
 import Swal from 'sweetalert2'
 import { CartContext } from '../../context/CartContext'
 import { generarOrden } from '../../firebase/GenerarOrden'
+import "../Checkout/Checkout.scss"
 
 export const Checkout = () => {
-
     const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext)
-
-    const [values, setValues] = useState({
-        nombre: '',
-        email: '',
-        tel: 0,
+    const [datosUsuario, setDatosUsuario] = useState({
+        nombre: "",
+        email: "",
+        telefono: "",
     })
-
-    const handleInputChange = (e) => {
-        setValues({
-            ...values,
-            [e.target.name]: e.target.value
+    const handleInputUser = (dato) => {
+        setDatosUsuario({
+            ...datosUsuario,
+            [dato.target.name]: dato.target.value
         })
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        if (values.nombre.length > 3 && values.email.length > 3 && values.tel.length > 5) {
-            generarOrden(values, carrito, precioTotal())
-                .then(res => {
+    const handleComprar = (event) => {
+        event.preventDefault()
+        if (datosUsuario.nombre.length > 3 && datosUsuario.email.length > 6 && datosUsuario.telefono.length > 8) {
+            generarOrden(datosUsuario, carrito, precioTotal())
+                .then(response => {
                     Swal.fire({
                         icon: 'success',
-                        title: 'Su compra fue registrada!',
-                        text: `Guarde este identificador: ${res}`,
-                        confirmButtonText: 'Genial!'
+                        title: 'Tu compra fue completada',
+                        text: `Tu número de ticket: ${response}`,
+                        confirmButtonText: 'Volver'
                     })
-
                     vaciarCarrito()
                 })
                 .catch(err => {
-                    console.log(err)
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error:',
+                        text: `${err}`,
+                    })
                 })
         } else {
             Swal.fire({
                 icon: 'error',
-                title: 'Campos inválidos',
-                text: 'Revise su información'
+                title: 'Falta información',
+                text: 'Revisa tus datos'
             })
         }
-
-
     }
 
-
-
     return (
-        <div>
-            <h2>Checkout</h2>
-            <hr />
-
+        <Container>
+            <h2 className="checkout_title">Checkout</h2>
+            <h4 className="checkout_title">Por favor, ingresa tus datos para cursar el pedido:</h4>
             {!carrito.length
                 ? <Redirect to="/" />
                 :
-
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <input
-                            type="text"
-                            value={values.nombre}
-                            onChange={handleInputChange}
-                            name="nombre"
-                            required
-                        />
-                        <input
-                            type="tel"
-                            value={values.tel}
-                            onChange={handleInputChange}
-                            name="tel"
-                            required
-                        />
-                        <input
-                            type="email"
-                            value={values.email}
-                            onChange={handleInputChange}
-                            name="email"
-                            required
-                        />
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
+                <>
+                    <Col className="checkout_col">
+                        <form className="checkout_form" onSubmit={handleComprar}>
+                            <Row className="checkout_row">
+                                <input
+                                    type="text"
+                                    value={datosUsuario.nombre}
+                                    onChange={handleInputUser}
+                                    name="nombre"
+                                    placeholder="Tu nombre"
+                                    required
+                                />
+                            </Row>
+                            <Row className="checkout_row">
+                                <input
+                                    type="text"
+                                    value={datosUsuario.telefono}
+                                    onChange={handleInputUser}
+                                    name="telefono"
+                                    placeholder="Tu teléfono"
+                                    required
+                                />
+                            </Row>
+                            <Row className="checkout_row">
+                                <input
+                                    type="email"
+                                    value={datosUsuario.email}
+                                    onChange={handleInputUser}
+                                    name="email"
+                                    placeholder="email"
+                                    required
+                                />
+                            </Row>
+                            <Row className="checkout_row">
+                                <button type="submit" className="btn btn-outline-success checkout_btn">Comprar</button>
+                            </Row>
+                        </form>
+                    </Col>
+                </>
             }
-
-        </div>
+        </Container>
     )
 }
