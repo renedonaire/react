8e12ce484cd53516f5afd20ef8b3bd2361b88234
René Donaire <rene.donaire@gmail.com>
2021-10-01 20:08:20 -0300

    Se agrega "loading" al formulario

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,27 @@
 import React, { useState, useContext } from "react"
+import { Container, Row, Col, Spinner } from 'react-bootstrap'
 import Swal from 'sweetalert2'
 import { generarOrden } from '../../firebase/GenerarOrden'
 import { CartContext } from '../../context/CartContext'
+import { UIContext } from '../../context/UIContext'
 import "./Form.scss"
 
 export const Form = () => {
   const { carrito, vaciarCarrito, precioTotal } = useContext(CartContext)
-
   const intialValues = { nombre: "", telefono: "", email_1: "", email_2: "" };
   const [formValues, setFormValues] = useState(intialValues)
   const [formErrors, setFormErrors] = useState({})
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const { loading, setLoading } = useContext(UIContext)
 
   const submit = () => {
-    console.log(formValues)
+    setLoading(true)
     generarOrden(formValues, carrito, precioTotal())
       .then(response => {
+        setLoading(false)
         Swal.fire({
           icon: 'success',
-          title: `${formValues.name} tu compra fue completada`,
+          title: `${formValues.nombre} , compra completada`,
           text: `Tu número de ticket: ${response}`,
           confirmButtonText: 'Volver'
         })
@@ -33,13 +36,11 @@ export const Form = () => {
       })
   }
 
-  //input change handler
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormValues({ ...formValues, [name]: value });
   }
 
-  //form submission handler
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormErrors(validate(formValues));
@@ -49,7 +50,6 @@ export const Form = () => {
     }
   }
 
-  //form validation handler
   const validate = (values) => {
     let errors = {};
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
@@ -72,7 +72,7 @@ export const Form = () => {
       errors.email_1 = "Formato de email inválido";
     }
 
-        if (!values.email_2) {
+    if (!values.email_2) {
       errors.email_2 = "Repite tu email";
     } else if (!(values.email_2 === values.email_1)) {
       errors.email_2 = "Los email no concuerdan";
@@ -82,127 +82,85 @@ export const Form = () => {
   }
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit} noValidate>
-        <div className="form-row">
-          <label htmlFor="nombre">Nombre</label>
-          <input
-            type="text"
-            name="nombre"
-            id="nombre"
-            value={formValues.nombre}
-            onChange={handleChange}
-            className={formErrors.nombre && "input-error"}
-          />
-          {formErrors.nombre && (
-            <span className="error">{formErrors.nombre}</span>
-          )}
-        </div>
+    <>
+      {loading
+        ?
+        <Container className="ItemListContainer_spinner">
+          <Row>
+            <Col ><h4>Procesando...</h4></Col>
+            <Col><Spinner animation="grow" variant="danger" /></Col>
+          </Row>
+        </Container>
+        :
+        <Col className="form_col">
+          <form className="form_form" onSubmit={handleSubmit} noValidate>
+            <Row className="form_row">
+              <input
+                placeholder="Nombre"
+                type="text"
+                name="nombre"
+                id="nombre"
+                value={formValues.nombre}
+                onChange={handleChange}
+                className={formErrors.nombre && "input-error"}
+              />
+              {formErrors.nombre && (
+                <span className="form_error">{formErrors.nombre}</span>
+              )}
+            </Row>
 
-        <div className="form-row">
-          <label htmlFor="telefono">Teléfono</label>
-          <input
-            type="text"
-            name="telefono"
-            id="telefono"
-            value={formValues.telefono}
-            onChange={handleChange}
-            className={formErrors.telefono && "input-error"}
-          />
-          {formErrors.telefono && (
-            <span className="error">{formErrors.telefono}</span>
-          )}
-        </div>
+            <Row className="form_row">
+              <input
+                placeholder="Teléfono"
+                type="text"
+                name="telefono"
+                id="telefono"
+                value={formValues.telefono}
+                onChange={handleChange}
+                className={formErrors.telefono && "input-error"}
+              />
+              {formErrors.telefono && (
+                <span className="form_error">{formErrors.telefono}</span>
+              )}
+            </Row>
 
-                <div className="form-row">
-          <label htmlFor="email_1">Email</label>
-          <input
-            type="email"
-            name="email_1"
-            id="email_1"
-            value={formValues.email_1}
-            onChange={handleChange}
-            className={formErrors.email_1 && "input-error"}
-          />
-          {formErrors.email_1 && (
-            <span className="error">{formErrors.email_1}</span>
-          )}
-        </div>
+            <Row className="form_row">
+              <input
+                placeholder="E-mail"
+                type="email"
+                name="email_1"
+                id="email_1"
+                value={formValues.email_1}
+                onChange={handleChange}
+                className={formErrors.email_1 && "input-error"}
+              />
+              {formErrors.email_1 && (
+                <span className="form_error">{formErrors.email_1}</span>
+              )}
+            </Row>
 
-                <div className="form-row">
-          <label htmlFor="email_2">Confirma tu email</label>
-          <input
-            type="email"
-            name="email_2"
-            id="email_2"
-            value={formValues.email_2}
-            onChange={handleChange}
-            className={formErrors.email_2 && "input-error"}
-          />
-          {formErrors.email_2 && (
-            <span className="error">{formErrors.email_2}</span>
-          )}
-        </div>
+            <Row className="form_row">
+              <input
+                placeholder="Confirma tu e-mail"
+                type="email"
+                name="email_2"
+                id="email_2"
+                value={formValues.email_2}
+                onChange={handleChange}
+                className={formErrors.email_2 && "input-error"}
+              />
+              {formErrors.email_2 && (
+                <span className="form_error">{formErrors.email_2}</span>
+              )}
+            </Row>
 
-        <button type="submit">Sign In</button>
-      </form>
-    </div>
+            <Row className="form_row">
+              <button type="submit" className="btn btn-success form_btn">Hacer pedido</button>
+            </Row>
+
+          </form>
+        </Col>
+      }
+    </>
   )
 }
-
-
-{/* <Col className="checkout_col">
-                            <form className="checkout_form" onSubmit={handleComprar}>
-                                <Row className="checkout_row">
-                                    <label for="nombre">Nombre</label>
-                                    <input
-                                        className={claseNombre}
-                                        type="text"
-                                        value={datosUsuario.nombre}
-                                        onChange={handleInputUser}
-                                        name="nombre"
-                                        placeholder="Tres o más caracteres"
-                                        required
-                                    />
-                                </Row>
-                                <Row className="checkout_row">
-                                    <label for="telefono">Teléfono</label>
-                                    <input
-                                        className={claseTelefono}
-                                        type="text"
-                                        value={datosUsuario.telefono}
-                                        onChange={handleInputUser}
-                                        name="telefono"
-                                        placeholder="Ocho o más caracteres"
-                                        required
-                                    />
-                                </Row>
-                                <Row className="checkout_row">
-                                    <label for="email_1">email</label>
-                                    <input
-                                        className={claseEmail_1}
-                                        type="email"
-                                        value={datosUsuario.email_1}
-                                        onChange={handleInputUser}
-                                        name="email_1"
-                                        placeholder="email"
-                                        required
-                                    />
-                                </Row>
-                                <Row className="checkout_row">
-                                    <label for="email_2">Repite tu email</label>
-                                    <input
-                                        className={claseEmail_2}
-                                        type="email"
-                                        value={datosUsuario.email_2}
-                                        onChange={handleInputUser}
-                                        name="email_2"
-                                        placeholder="Repite tu email"
-                                        required
-                                    />
-                                </Row>
-                                <Row className="checkout_row">
-                                    <button type="submit" className="btn btn-outline-success checkout_btn">Comprar</button>
-                                </Row>
-                            </form>
-                        </Col> */}
