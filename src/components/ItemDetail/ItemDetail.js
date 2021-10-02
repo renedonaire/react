3ae8a289b7235f8c1f2d3541d4bb0de36845458d
René Donaire <rene.donaire@gmail.com>
2021-10-01 21:49:57 -0300

    Soluciona redireccionamiento

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -10,36 +10,57 @@ export const ItemDetail = ({ id, title, category, description, description_full,
     const { agregarAlCarrito, isInCart } = useContext(CartContext)
     const [cantidad, setCantidad] = useState(1)
     const handleAdd = () => { agregarAlCarrito({ category, id, title, description, pictureUrl, price, cantidad }) }
-    const pricePesos = price.toLocaleString('de-DE')
+    const pricePesos = () => {
+        if (price) {
+            return price.toLocaleString('de-DE')
+        } else {
+            return false
+        }
+    }
 
     return (
-        <Container>
-            <h2 className='item_detail_title' >{title}</h2>
-            <Row className="item_detail_row">
-                <Col className="item_detail_book" lg={4}><img className='item_detail_image' src={pictureUrl} alt={title} /></Col>
-                <Col>
-                    <Row>
-                        <p className='item_detail_description'>{description_full}<br/>Stock: {stock}</p>
-                    </Row>
-                    <Row>
+        <>
+            {pricePesos()
+                ?
+                <Container>
+                    <h2 className='item_detail_title' >{title}</h2>
+                    <Row className="item_detail_row">
+                        <Col className="item_detail_book" lg={4}><img className='item_detail_image' src={pictureUrl} alt={title} /></Col>
                         <Col>
-                            <Link to={`/category/${category}`} className="btn btn-outline-dark">Volver</Link>
-                        </Col>
-                        <Col>
-                            <Row><p className='item_detail_price'>${pricePesos}</p></Row>
                             <Row>
-                                <Counter
-                                    max={stock}
-                                    cantidad={cantidad}
-                                    setCantidad={setCantidad}
-                                    agregar={handleAdd}
-                                    agregado={isInCart(id)}
-                                />
+                                <p className='item_detail_description'>{description_full}<br />Stock: {stock}</p>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Link to={`/category/${category}`} className="btn btn-outline-dark">Volver</Link>
+                                </Col>
+                                <Col>
+                                    <Row><p className='item_detail_price'>${pricePesos}</p></Row>
+                                    <Row>
+                                        <Counter
+                                            max={stock}
+                                            cantidad={cantidad}
+                                            setCantidad={setCantidad}
+                                            agregar={handleAdd}
+                                            agregado={isInCart(id)}
+                                            />
+                                    </Row>
+                                </Col>
                             </Row>
                         </Col>
                     </Row>
+                </Container>
+
+                :
+                <Col>
+                    <Row>
+                        <h2>Error</h2>
+                    </Row>
+                    <Row>
+                        <Link to={`/`} className="btn btn-outline-dark">Inicio</Link>
+                    </Row>
                 </Col>
-            </Row>
-        </Container>
+            }
+        </>
     )
 }
